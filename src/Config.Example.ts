34ee11d19f13449ca550649135be2aea7db0c8c0
CyclerOfLife <sophiapolor@gmail.com>
2020-11-14 13:54:48 +0100

    adding config

---
+++ src/Config.Example.ts
@@ -0,0 +1,11 @@
+export let token: string = '';
+export let owners: string[] = [
+	'699681088301301841',
+	'582245573415927819',
+];
+export let dbName: string = 'db';
+export let nodes: any = [
+	{ host: 'localhost', port: 2333, password: 'youshallnotpass' },
+];
+export let db: any = 'hi';
+
